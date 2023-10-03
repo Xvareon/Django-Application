55082d8ec1b8a6f17b765be49fc249a6cd11df22
Gavs <poOdrag0n69@gmail.com>
2023-10-03 18:32:32 +0800

    feat: Added create functionality

--- django_app/webapp/urls.py
+++ django_app/webapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from webapp import views
 
 urlpatterns = [
-
+    path('products', views.index, name="index"),
+    path('products/create', views.create, name="create")
 ]
--- django_app/webapp/views.py
+++ django_app/webapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 
 # Create your views here.
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, "index.html", {'products': products})
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        variant = request.POST.get('variant')
+        qty = request.POST.get('qty')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+
+        Product.objects.create(
+            name=name,
+            variant=variant,
+            qty=qty,
+            price=price,
+            description=description
+        )
+
+        return redirect('index')
+
+    return render(request, "create.html")
